b7bcdceecc19b99c0666bff26481c26780df026b
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-10-13 13:41:21 -0700

    Added service implementation to product.service.ts

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {} //inject http client
+
+  //returns an observable
+  //Map the JSON data from Spring Data REST to Product array
+  getProductList(): Observable<Product[]> {
+    return this.httpClient
+      .get<GetResponse>(this.baseUrl)
+      .pipe(map((response) => response._embedded.products));  //map is a special operator from the RXJS (Reactive JavaScript) module
+  }
+}
+
+// add supporting interface to help with mapping
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  };
+  /* 
+interface getresponse will help us unwrap the JSON
+data from the spring of data REST API and make use
+of that underscore embedded entry that comes back
+from the spring data REST API.
+*/
 }
