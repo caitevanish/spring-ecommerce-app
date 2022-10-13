a16401856e8f22caf0d343867fb2eda2a9c197b5
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-10-13 13:58:02 -0700

    Added CrossOrigin annotation

--- 02-backend/src/main/java/com/evanishyn/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/src/main/java/com/evanishyn/ecommerce/dao/ProductCategoryRepository.java
@@ -3,12 +3,15 @@ package com.evanishyn.ecommerce.dao;
 import com.evanishyn.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 //productCategory=name of JSON Entry
 //product-category will be the path
 
 //entity is ProductCategory, the product key is long
 
+//accept calls from web browser scripts from this origin
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path="product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- 02-backend/src/main/java/com/evanishyn/ecommerce/dao/ProductRepository.java
+++ 02-backend/src/main/java/com/evanishyn/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.evanishyn.ecommerce.dao;
 
 import com.evanishyn.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")       //accept calls from web browser scripts from this origin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
