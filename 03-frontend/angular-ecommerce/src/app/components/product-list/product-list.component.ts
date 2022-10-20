873aaf3261b4d59fc39a075dd895da997c0ddcb8
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-10-20 15:54:12 -0700

    Added html template, creating new grid for product list  component html

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -4,26 +4,26 @@ import { Product } from 'src/app/common/product';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
+  // templateUrl: './product-list-table.component.html',
   // templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
   products: Product[] = [];
 
-  constructor(private productService: ProductService) { } //Inject our product service into constructor/component
+  constructor(private productService: ProductService) {} //Inject our product service into constructor/component
 
   ngOnInit(): void {
-    this.listProducts();  //similar to @PostConstruct; add hook to call listProducts method
+    this.listProducts(); //similar to @PostConstruct; add hook to call listProducts method
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe( //method is invoked once you subscribe
-      data => {
+    this.productService.getProductList().subscribe(
+      //method is invoked once you subscribe
+      (data) => {
         this.products = data;
       }
-    )  
+    );
   }
-
 }
