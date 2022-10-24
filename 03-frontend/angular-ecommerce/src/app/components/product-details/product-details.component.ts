b62c1177f68d29cf9fee72dd69461ae746799259
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-10-24 12:29:53 -0700

    lesson 101: enhance details component;

--- 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,15 +1,33 @@
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+  product!: Product;
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      //subscribe on the params and then make a call to a method handle product details.
+      this.handleProductDetails();
+    });
   }
+  handleProductDetails() {
+    //get the id param string. convert string to a number
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
 
+    this.productService.getProduct(theProductId).subscribe((data) => {
+      this.product = data;
+    });
+  }
 }
