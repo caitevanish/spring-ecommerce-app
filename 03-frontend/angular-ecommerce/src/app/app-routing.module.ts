10f2cf07cea79b672810451d0d69af7703f23fd7
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-10-24 11:26:15 -0700

    lesson 99: Setting up product details component and routes

--- 03-frontend/angular-ecommerce/src/app/app-routing.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 const routes: Routes = [
+  { path: 'products/:id', component: ProductDetailsComponent },
   { path: 'search/:keyword', component: ProductListComponent },
   { path: 'category/:id/:name', component: ProductListComponent }, //start from the most specific route all the way down to the most generic route
   { path: 'category', component: ProductListComponent },
--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -8,13 +8,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
-import { SearchComponent } from './components/search/search.component'; //manually type this in
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, PageNotFoundComponent, ProductCategoryMenuComponent, SearchComponent],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    PageNotFoundComponent,
+    ProductCategoryMenuComponent,
+    SearchComponent,
+    ProductDetailsComponent,
+  ],
   imports: [
     BrowserModule,
-    HttpClientModule,  //service module provided by Angular that allows us to perform HTTP requests and easily manipulate those requests and their responses.
+    HttpClientModule, //service module provided by Angular that allows us to perform HTTP requests and easily manipulate those requests and their responses.
     AppRoutingModule,
   ],
   providers: [ProductService], //add refernce to our ProductService. Allows us to inject given service into other parts of our application
