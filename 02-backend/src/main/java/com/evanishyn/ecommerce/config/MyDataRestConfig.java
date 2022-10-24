ff561cb383c9d97db349514f0a7ef24314333bb9
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-10-24 08:52:13 -0700

    backend: updated config file to expose entity id

--- 02-backend/src/main/java/com/evanishyn/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/src/main/java/com/evanishyn/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,29 @@ package com.evanishyn.ecommerce.config;
 
 import com.evanishyn.ecommerce.entity.Product;
 import com.evanishyn.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired      //autowire JPA entity manager
+    public MyDataRestConfig(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
@@ -28,7 +43,29 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
+        //call an internal helper method
+        exposeIds(config);
     }
+
+    private void exposeIds(RepositoryRestConfiguration config){
+         //expose entity ids
+
+        //1. gets a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        //2. create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        //3. get the entity types for the entities
+        for(EntityType tempEntityType: entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        //4. expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+
 }
 
 //in postman: 405Method Not Allowed
