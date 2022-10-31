2e5b321a0a364ee017b5ef05ea4615ff7cabfae6
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-10-31 13:11:09 -0700

    136. Added table html. 137. Added code to cartDetailsComponent.ts

--- 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -1,15 +1,34 @@
+import { CartService } from './../../services/cart.service';
+import { CartItem } from './../../common/cart-item';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
-  styleUrls: ['./cart-details.component.css']
+  styleUrls: ['./cart-details.component.css'],
 })
 export class CartDetailsComponent implements OnInit {
+  CartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
-  constructor() { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    this.listCartDetails();
   }
+  listCartDetails() {
+    //get a handle to cart items
+    this.CartItems = this.cartService.cartItems;
 
+    //subscribe to the cart totalPrice
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
+
+    //subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data)
+    );
+    //compute cart total price and quantity
+    this.cartService.computeCartTotals();
+  }
 }
