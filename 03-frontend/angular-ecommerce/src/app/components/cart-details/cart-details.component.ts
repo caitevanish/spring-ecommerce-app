e54910826388a5c8be2718a6596f6778a22dac33
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-11-03 10:04:52 -0700

    140. Empty cart display. 141. Increment button html. 143. button functionality

--- 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -31,4 +31,8 @@ export class CartDetailsComponent implements OnInit {
     //compute cart total price and quantity
     this.cartService.computeCartTotals();
   }
+
+  incrementQuantity(theCartItem: CartItem) {
+    this.cartService.addToCart(theCartItem);
+  }
 }
