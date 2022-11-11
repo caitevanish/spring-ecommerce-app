de4d319ee6af264509a677114b83ed26064c766a
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-11-11 12:38:34 -0800

    154. Added billingAddress, Created copy Shipping to billing address method!!

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -44,6 +44,16 @@ export class CheckoutComponent implements OnInit {
     });
   }
 
+  copyShippingAddressToBillingAddress(event) {
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls['billingAddress'].setValue(
+        this.checkoutFormGroup.controls['shippingAddress'].value
+      );
+    } else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+  }
+
   onSubmit() {
     console.log('Handling the submit button');
     console.log(this.checkoutFormGroup.get('customer')?.value);
